89a4616ab4150595997312410b02989e2d90f62b
Fernando Boueres <boueres.fernando@gmail.com>
2025-05-07 12:31:58 +0000

    init.sql

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE USER 'fernando.boueres'@'%' IDENTIFIED BY 'Overlying6-Powdering8-Twice5-Dyslexia0-Handgrip6';
+CREATE USER 'francisco.passos'@'%' IDENTIFIED BY 'Oops2-Sequence6-Cardstock6-Handcuff1-Suitcase8';
+CREATE USER 'luis.costa'@'%' IDENTIFIED BY 'Eject6-Stilt2-Poem2-Boss4-Payphone3';
+CREATE USER 'kaua.aragao'@'%' IDENTIFIED BY 'Pouring8-Expel5-Startle6-Refresh6-Ascension8';
+
+GRANT ALL PRIVILEGES ON *.* TO 'fernando.boueres'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'francisco.passos'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'luis.costa'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'kaua.aragao'@'%' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
